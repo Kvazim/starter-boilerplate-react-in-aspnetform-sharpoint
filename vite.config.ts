88176f5a5a8cd1ddf2d78f8748b9630adc450215
Meshcheryakov Vladimir <89020510+Kvazim@users.noreply.github.com>
2025-03-18 12:00:06 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-/// <reference types='vitest' />
+/// <reference types="vitest/config" />
 /// <reference types='vite/client' />
 
 import { defineConfig } from 'vite';
@@ -6,7 +6,22 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // base: '/app-pages/React/react/',
   plugins: [react()],
+  server: {
+    cors: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
   test: {
     globals: true,
     environment: 'jsdom',
