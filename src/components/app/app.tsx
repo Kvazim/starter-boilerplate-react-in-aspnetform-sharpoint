9218a035f355a7ab7238890651600d331d1ed48c
Meshcheryakov Vladimir <89020510+Kvazim@users.noreply.github.com>
2025-03-18 12:11:48 +0300

    Update app.tsx

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,74 +1,17 @@
-import MemoizedLogin from '../../pages/login/login';
-import { HelmetProvider } from 'react-helmet-async';
-import MemoizedMain from '../../pages/main/main';
-import { Route, Routes } from 'react-router-dom';
-import { AppRoute, AuthorizationStatus, Status } from '../../const';
-import Layout from '../layout/layout';
-import PageNotFound from '../../pages/page-not-found/page-not-found';
-import Favorites from '../../pages/favorites/favorites';
-import PrivateRoute from '../private-route/private-route';
-import { useAppDispatch, useAppSelector } from '../../hooks';
-import LoadingScreen from '../loading-screen/loading-screen';
-import HistoryRouter from '../history-route/history-route';
-import browserHistory from '../../browser-history';
-import { getAuthorizationStatus } from '../../store/user-process/user-process.selectors';
-import { getOffersLoadingStatus } from '../../store/offer-process/offer-process.selectors';
-import { useEffect } from 'react';
-import { fetchFavoriteOffersAction, fetchOffersAction } from '../../store/api-actions';
-import ErrorLoadSreen from '../error-load-screen/error-load-screen';
-import MemoizedOfferPage from '../../pages/offer-page/offer-page';
+import { spfi, SPBrowser } from "@pnp/sp";
 
-function App(): JSX.Element {
-  const dispatch = useAppDispatch();
-  const isAuthChecked = useAppSelector(getAuthorizationStatus);
-  const isDataLoading = useAppSelector(getOffersLoadingStatus);
+import "@pnp/sp/sites";
+import "@pnp/sp/webs";
+import "@pnp/sp/context-info";
+import "@pnp/sp/site-users/web";
 
-  useEffect(() => {
-    if (isAuthChecked === AuthorizationStatus.Auth) {
-      dispatch(fetchFavoriteOffersAction());
-    }
-  }, [dispatch, isAuthChecked]);
+async function App() {
+  const url = window.location.origin
 
-  if (isAuthChecked === AuthorizationStatus.Unknown || isDataLoading === Status.Loading) {
-    return (
-      <LoadingScreen />
-    );
-  }
-
-  if (isDataLoading === Status.Failed) {
-    return (
-      <ErrorLoadSreen onButtonDispatchClick={fetchOffersAction} />
-    );
-  }
+  console.log(await spfi(url).using(SPBrowser()).web.currentUser())
 
   return (
-    <HelmetProvider>
-      <HistoryRouter history={browserHistory}>
-        <Routes>
-          <Route path={AppRoute.Root} element={<Layout />}>
-            <Route index element={<MemoizedMain />} />
-            <Route
-              path={AppRoute.Login}
-              element={
-                <PrivateRoute authorizationStatus={isAuthChecked} isReverse>
-                  <MemoizedLogin />
-                </PrivateRoute>
-              }
-            />
-            <Route path={`${AppRoute.Offer}:id`} element={<MemoizedOfferPage />} />
-            <Route
-              path={AppRoute.Favorites}
-              element={
-                <PrivateRoute authorizationStatus={isAuthChecked}>
-                  <Favorites />
-                </PrivateRoute>
-              }
-            />
-            <Route path="*" element={<PageNotFound />} />
-          </Route>
-        </Routes>
-      </HistoryRouter>
-    </HelmetProvider>
+    <h1>Hello World</h1>
   );
 }
 
