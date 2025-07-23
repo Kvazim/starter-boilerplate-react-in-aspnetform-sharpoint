029858bb62b06d65b89e7b94bfeb7dc11ddd01ed
Meshcheryakov Vladimir <89020510+Kvazim@users.noreply.github.com>
2025-07-23 17:24:52 +0300

    Update app.tsx

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,12 +1,6 @@
-import { useErrorLog } from '../features';
-import { Header } from '../widgets/header/header';
-
 function App() {
-  useErrorLog();
-
   return (
     <div className="main-container">
-      <Header />
       <main id="react-content" className="react-content"></main>
     </div>
   );
